6e634d5678252e98e5e5cfce03de52e5a82d4b66
Pallab6Sense <pallab@6sensehq.com>
2023-03-20 14:01:32 +0600

    Menu Component done 2:01 PM

--- pages/menu.js
+++ pages/menu.js
@@ -1,7 +1,183 @@
-export default function Menu(){
-    return(
-        <>
-        menu
-        </>
-    )
+import Navigation from '@/Components/Navigation';
+import {
+  MailOutlined,
+  SettingOutlined,
+  AppstoreOutlined,
+} from '@ant-design/icons';
+import { Menu, Button, Switch,Divider } from 'antd';
+import { useState } from 'react';
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+const items = [
+  getItem('Navigation One', 'sub1', <MailOutlined />, [
+    getItem(
+      'Item 1',
+      'g1',
+      null,
+      [getItem('Option 1', '1'), getItem('Option 2', '2')],
+      'group'
+    ),
+    getItem(
+      'Item 2',
+      'g2',
+      null,
+      [getItem('Option 3', '3'), getItem('Option 4', '4')],
+      'group'
+    ),
+  ]),
+  getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
+    getItem('Option 5', '5'),
+    getItem('Option 6', '6'),
+    getItem('Submenu', 'sub3', null, [
+      getItem('Option 7', '7'),
+      getItem('Option 8', '8'),
+    ]),
+  ]),
+  {
+    type: 'divider',
+  },
+  getItem('Navigation Three', 'sub4', <SettingOutlined />, [
+    getItem('Option 9', '9'),
+    getItem('Option 10', '10'),
+    getItem('Option 11', '11'),
+    getItem('Option 12', '12'),
+  ]),
+  getItem(
+    'Group',
+    'grp',
+    null,
+    [getItem('Option 13', '13'), getItem('Option 14', '14')],
+    'group'
+  ),
+];
+
+const rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+
+export default function Me_nu() {
+  //   const onClick = (e) => {
+  //     console.log('click ', e);
+  //   };
+
+  const [collapsed, setCollapsed] = useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const [openKeys, setOpenKeys] = useState(['sub1']);
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
+
+  const [theme, setTheme] = useState('dark');
+  const [current, setCurrent] = useState('1');
+  const changeTheme = (value) => {
+    setTheme(value ? 'dark' : 'light');
+  };
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  const [mode, setMode] = useState('inline');
+  const changeMode = (value) => {
+    setMode(value ? 'vertical' : 'inline');
+  };
+
+
+  return (
+    <>
+      <div className="container">
+        <Navigation></Navigation>
+        <div className="menuContainer">
+          <div className="menuExample1">
+            <Menu
+              onClick={onClick}
+              style={{
+                width: 350,
+              }}
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              mode="inline"
+              items={items}
+            />
+          </div>
+
+          <div className="menuExample2">
+            <Menu
+              onClick={onClick}
+              style={{
+                width: 256,
+              }}
+              mode="vertical"
+              items={items}
+            />
+          </div>
+
+          <div className="menuExample3">
+            <Menu
+              mode="inline"
+              openKeys={openKeys}
+              onOpenChange={onOpenChange}
+              style={{
+                width: 256,
+              }}
+              items={items}
+            />
+          </div>
+
+          <div className="menuExample4">
+            <Switch
+              checked={theme === 'dark'}
+              onChange={changeTheme}
+              checkedChildren="Dark"
+              unCheckedChildren="Light"
+            />
+            <br />
+            <br />
+            <Menu
+              theme={theme}
+              onClick={onClick}
+              style={{
+                width: 256,
+              }}
+              defaultOpenKeys={['sub1']}
+              selectedKeys={[current]}
+              mode="inline"
+              items={items}
+            />
+          </div>
+
+          <div className="menuExample5">
+          <Switch onChange={changeMode} /> Change Mode
+      <Divider type="vertical" />
+      {/* <Switch onChange={changeTheme} /> Change Style */}
+      <br />
+      <br />
+      <Menu
+        style={{
+          width: 256,
+        }}
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        mode={mode}
+        theme={theme}
+        items={items}
+      />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
