6a7abddebe64ad41ea66a93fbe51ca1d09d01fd0
Pallab6Sense <pallab@6sensehq.com>
2023-03-20 15:15:43 +0600

    Pagination done

--- pages/pagination.js
+++ pages/pagination.js
@@ -1,7 +1,43 @@
-export default function Pagination(){
-    return(
-        <>
-        pagination
-        </>
-    )
+import Navigation from '@/Components/Navigation';
+import { Pagination } from 'antd';
+
+export default function Pagi_nation() {
+  const onChange = (pageNumber) => {
+    console.log('Page: ', pageNumber);
+  };
+
+  const itemRender = (_, type, originalElement) => {
+    if (type === 'prev') {
+      return <a>Previous</a>;
+    }
+    if (type === 'next') {
+      return <a>Next</a>;
+    }
+    return originalElement;
+  };
+  return (
+    <>
+      <div className="container">
+        <Navigation></Navigation>
+        <div className="paginationContainer">
+          <div className="paginationExample1">
+            <Pagination defaultCurrent={1} total={50} />
+          </div>
+
+          <div className="paginationExample2">
+            <Pagination
+              showQuickJumper
+              defaultCurrent={2}
+              total={500}
+              onChange={onChange}
+            />
+          </div>
+
+          <div className="paginationExample3">
+            <Pagination total={500} itemRender={itemRender}></Pagination>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
