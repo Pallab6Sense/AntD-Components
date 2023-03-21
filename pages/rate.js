7f7302c33f03962bab28d63f95888b76e4a34ef8
Pallab6Sense <pallab@6sensehq.com>
2023-03-21 10:06:52 +0600

    Rate Component 10:06 AM

--- pages/rate.js
+++ pages/rate.js
@@ -1,6 +1,13 @@
 import Navigation from '@/Components/Navigation';
 import { HeartOutlined } from '@ant-design/icons';
-
+import { FrownOutlined, MehOutlined, SmileOutlined } from '@ant-design/icons';
+const customIcons = {
+  1: <FrownOutlined />,
+  2: <FrownOutlined />,
+  3: <MehOutlined />,
+  4: <SmileOutlined />,
+  5: <SmileOutlined />,
+};
 import { Rate } from 'antd';
 const Ra_te = () => {
   return (
@@ -9,9 +16,7 @@ const Ra_te = () => {
         <Navigation></Navigation>
 
         <div className="rateContainer">
-          <div className="rateExample1">
-            <Rate />
-          </div>
+          <div className="rateExample1"></div>
           <div className="rateExample2">
             <Rate allowHalf defaultValue={2.5} />
           </div>
@@ -28,6 +33,23 @@ const Ra_te = () => {
             />
             <br />
             <Rate character="好" allowHalf />
+          </div>
+
+          <div className="rateExample4">
+            <Rate defaultValue={3} />
+            <span className="ant-rate-text">allowClear: true</span>
+            <br />
+            <Rate allowClear={false} defaultValue={3} />
+            <span className="ant-rate-text">allowClear: false</span>
+          </div>
+
+          <div className="rateExample">
+            <Rate defaultValue={2} character={({ index }) => index + 1} />
+            <br />
+            <Rate
+              defaultValue={3}
+              character={({ index }) => customIcons[index + 1]}
+            />
           </div>
         </div>
       </div>
